981da38d42b11972dfaa5ef446a26540b4e1d144
Denis Ryzhkov <denis@academicmerit.com>
2020-10-01 16:50:16 +0300

    0.1.2: Fixed relative imports

--- binfra.sh
+++ binfra.sh
@@ -4,9 +4,9 @@ IFS=$'\n\t'
 
 # Entry point of binfra lib
 
-import lib/aws/profile
-import lib/aws/ssm
-import lib/aws/vpc
-import lib/aws/rds
-import lib/aws/fargate
-import lib/aws/expose
+import ./lib/aws/profile
+import ./lib/aws/ssm
+import ./lib/aws/vpc
+import ./lib/aws/rds
+import ./lib/aws/fargate
+import ./lib/aws/expose
